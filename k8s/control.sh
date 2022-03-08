bcdb3af38bd52c70fb2c0e21b0df116e69bbf2ed
bomkvilt <kvilt-d.max@yandex.ru>
2022-03-08 19:59:23 +0300

    local kubernetes deployment

--- k8s/control.sh
+++ k8s/control.sh
@@ -4,4 +4,9 @@ if [ "$1" == "deploy-all" ]; then
     kubectl apply -f ./ovpn-configs.yaml
     kubectl apply -f ./ovpn-deployment.yaml
     kubectl apply -f ./ovpn-service.yaml
+elif [ "$1" == "clean" ]; then
+    kubectl delete -f ./ovpn-service.yaml
+    kubectl delete -f ./ovpn-deployment.yaml
+    kubectl delete -f ./ovpn-configs.yaml
+    kubectl delete -f ./ovpn-data.yaml
 fi
